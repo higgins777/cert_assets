3f7f489733c828c80f767589d60decf5aa4b8a8b
James <jamesh@bocatc.org>
2021-05-20 16:25:49 -0500

    fonts

--- js/info_popovers.js
+++ js/info_popovers.js
@@ -1,5 +1,5 @@
 function popover_content(id, title, content) {
-  return '<a tabindex="0" id="' +id+'" class="btn btn-lg btn-dark" role="button" data-bs-toggle="popover" data-bs-trigger="focus" title="'+title+'" data-bs-content=" ' + content + ' "><i class="bi bi-info-circle"></i></a>'
+  return '<a tabindex="0" id="' +id+'" style="margin-left: 5px; font-size: 2.2rem; cursor: hover" role="button" data-bs-toggle="popover" data-bs-trigger="focus" title="'+title+'" data-bs-content=" ' + content + ' "><i class="bi bi-info-circle"></i></a>'
 }
 
 function add_dashboard_popovers() {
@@ -27,5 +27,10 @@ function add_dashboard_popovers() {
   popover_id = 'announcements-info'
   announcements.append(popover_content(popover_id, "Announcements", "Info Stuff"))
   new bootstrap.Popover(document.getElementById(popover_id), {html:true})
-
 }
+
+$(document).ajaxComplete(function() {
+  if ($("#todo-info").length == 0) {
+      add_dashboard_popovers()
+  }
+});
