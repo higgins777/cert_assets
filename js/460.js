f74e5f8d2030813702df5ddfbf92e63d6d88e1cf
James <jamesh@bocatc.org>
2021-07-01 14:10:06 -0500

    materialize selects

--- js/460.js
+++ js/460.js
@@ -1,4 +1,27 @@
 $(document).ready(function(){
   $('.tooltipped').tooltip();
   $('select').formSelect();
+  create_menu()
+});
+
+function create_menu() {
+  var menu_nav = $("#BOCMenuNavLinks")
+
+  // Select all the menu elements (<li>) and split them in half
+  var menu_items = $("#BOCMenuNavLinks li")
+  var top_ul = $("#BOCMenuNavLinks ul").first()
+  
+  const half = Math.ceil(list.length / 2);
+  const firstHalf = list.slice(0, half)
+  const secondHalf = list.slice(-half)
+
+  if (firstHalf.length > 0) {
+    top_ul.html("<div id='boc-menu-col' class='boc-menu-col'></div><div id='boc-menu-col1' class='boc-menu-col'></div>")
+    $("#boc-menu-col0").html(firstHalf);
+    $("#boc-menu-col1").html(secondHalf);
+  }
+}
+
+$(function () {
+  create_menu()
 });
--- sql/SD_SUB_VARS/BOCATC_MY_PROFILE/BEST_ID.sql
+++ sql/SD_SUB_VARS/BOCATC_MY_PROFILE/BEST_ID.sql
@@ -1,4 +1,4 @@
-SELECT NVL2(c2.certificate_no, '<br><strong>Certification #:</strong> ' || c2.certificate_no, '<strong>Customer ID:</strong> ' || c1.cust_id)
+SELECT NVL2(c2.certificate_no, '<br><strong>Certification #:</strong> ' || c2.certificate_no, '<br><strong>Customer ID:</strong> ' || c1.cust_id)
 FROM cen_cust_mast c1
 LEFT JOIN crt_cust_mast c2 ON c2.cust_id=c1.cust_id AND c2.cert_ty = 'ATHLETIC_TRAINER'
 WHERE c1.cust_id = :p_cust_id
