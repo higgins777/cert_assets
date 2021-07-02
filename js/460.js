9bbc3c7b1c2ca3de9437542e0ec1112be7fb25fd
James <jamesh@bocatc.org>
2021-07-02 09:28:02 -0500

    dashboard specific styles

--- js/460.js
+++ js/460.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   $('.tooltipped').tooltip();
   $('select').formSelect();
+  add_dashboard_class();
 });
 
+function add_dashboard_class() {
+  var uri = window.location.href;
+  if (uri.includes('BOCATDASH')) {
+    $("#section-header").addClass("at-dashboard-container")
+  } else if (uri.includes('BOCAPDASH')) {
+    $("#section-header").addClass("ap-dashboard-container")
+  }
+}
