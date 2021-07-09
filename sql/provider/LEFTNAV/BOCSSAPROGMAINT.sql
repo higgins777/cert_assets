6a94359e930b4d61563d727dbf394c82b4861f11
James <jamesh@bocatc.org>
2021-07-09 13:24:22 -0500

    ap left nav

--- sql/provider/LEFTNAV/BOCSSAPROGMAINT.sql
+++ sql/provider/LEFTNAV/BOCSSAPROGMAINT.sql
@@ -1,4 +1,4 @@
-select '<a class="" href="ssaapexmnucall?p_app_id=BETASSA&p_page_id=53020&p_itemnames=P53020_PROGRAM_ID&p_itemvalues=' || o.program_id ||'" title="Program Details">' || program_nm || '  ' || o.start_dt || '<i class="material-icons">keyboard_arrow_right</i></a>'
+select '<a class="upcoming-program-link" href="ssaapexmnucall?p_app_id=BETASSA&p_page_id=53020&p_itemnames=P53020_PROGRAM_ID&p_itemvalues=' || o.program_id ||'" title="Program Details">' || program_nm || '  ' || o.start_dt || '<i class="material-icons upcoming-program-icon">keyboard_arrow_right</i></a>'
 from boc_vw_offering o
 where o.provider_id = bocdashboardlib.get_provider(:cust_id)
 and trunc(o.start_dt) >= trunc(sysdate)
--- sql/provider/LEFTNAV/BOCSSAPROGMAINTBTM.sql
+++ sql/provider/LEFTNAV/BOCSSAPROGMAINTBTM.sql
@@ -1,3 +1,3 @@
-select '<hr><br><a href="ssaapexmnucall?p_app_id=BETASSA&p_page_id=53015&p_itemnames=P53015_PROVIDER_ID&p_itemvalues=' || bocdashboardlib.get_provider(:cust_id) || '" title="View All Programs">View All Programs</a>
+select '<hr><a href="ssaapexmnucall?p_app_id=BETASSA&p_page_id=53015&p_itemnames=P53015_PROVIDER_ID&p_itemvalues=' || bocdashboardlib.get_provider(:cust_id) || '" title="View All Programs">View All Programs</a>
   <a href="sbmssamysubmittals.wizstep3?p_collection_id=PROGRAM&p_subcollection_cd=PROGRAM&p_cust_id=' || :cust_id || '&p_parent_cust_id=' || bocdashboardlib.get_provider(:cust_id) || '" title="Add New Program">Add New Program</a>'
 from dual
