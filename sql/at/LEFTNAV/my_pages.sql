878e3a5d1e290866b4c05a078258f88f67eb7af0
James <jamesh@bocatc.org>
2021-07-14 16:58:27 -0500

    apdash font-color

--- sql/at/LEFTNAV/my_pages.sql
+++ sql/at/LEFTNAV/my_pages.sql
@@ -1,3 +1,8 @@
 'https://cert.bocatc.org/bocdevssa/ssaapexmnucall?p_app_id=STDSSA&p_page_id=1000&p_itemvalues=BOCATDASH&p_cust_id=IND-177056'
 
 'https://cert.bocatc.org/bocdevssa/ssaapexmnucall?p_app_id=STDSSA&p_page_id=1000&p_itemvalues=BOCAPDASH&p_cust_id=IND-177056'
+
+
+https://cert.bocatc.org/bocssa/f?p=STDSSA:1000:4226859305841:::1000:P1000_DASHBOARD_CD,G_CUST_ID:BOCATDASH,BOC391820
+
+https://cert.bocatc.org/bocssa/f?p=STDSSA:1000:4226859305841:::1000:P1000_DASHBOARD_CD,G_CUST_ID:BOCAPDASH,IND-177056
