79a960f3a0245e18d5746776823be5f9e00c0aef
James <jamesh@bocatc.org>
2021-07-27 11:18:04 -0500

    ecc headers

--- js/740.js
+++ js/740.js
@@ -1,7 +1,13 @@
 $(document).ajaxComplete(function () {
   $("[id^=COL10_]").text("Documentation");
-  $("[id^=COL18_]").text("Completion Date");
-  $("[id^=COL19_]").text("CEUs");
+
+  if ($( ".t-Region-title:contains('ECC Documentation')" ).length > 0) {
+    $("[id^=COL17_]").text("Issue Date");
+    $("[id^=COL18_]").text("Expiration Date");
+  } else {
+    $("[id^=COL18_]").text("Completion Date");
+    $("[id^=COL19_]").text("CEUs");
+  }
 
   var page_link = $(".aaActive").first()
   if (page_link.length == 1) {
--- sql/at/dynamic_overviews/UPCOMING/register_for_exam.sql
+++ sql/at/dynamic_overviews/UPCOMING/register_for_exam.sql
@@ -12,19 +12,21 @@ select '<div class="boc-upcoming-row row">
 </div>'
 from dual
 where exists (
-  SELECT *
-  FROM SBM_SUBMITTAL s
-  INNER JOIN CRT_CUST_MAST m ON m.cust_id=s.primary_cust_id
+  SELECT 1
+  FROM CRT_CUST_MAST m
   INNER JOIN CRT_CUST_PERIOD p ON p.period_serno=m.period_serno
-  WHERE s.primary_cust_id = :cust_id
+  WHERE m.cust_id= :cust_id
   AND p.level_id = 'EXAM_ELIGIBLE'
-  AND p.start_dt > cencustlib.get_cust_attrdtl_dvar(p_cust_id => 'BOCATC', p_attribute_ty => 'AT_ELIGIBILITY_CLOSED', p_attribute_cd => 'AT_ELIGIBILITY_CLOSED')
+  AND m.cert_ty='ATHLETIC_TRAINER'
+  AND m.level_id='EXAM_ELIGIBLE'
+  AND p.start_dt < cencustlib.get_cust_attrdtl_dvar(p_cust_id => 'BOCATC', p_attribute_ty => 'AT_ELIGIBILITY_CLOSED', p_attribute_cd => 'AT_ELIGIBILITY_CLOSED')
   AND SYSDATE NOT BETWEEN 
     cencustlib.get_cust_attrdtl_dvar(p_cust_id => 'BOCATC', p_attribute_ty => 'EXAM_WIN_START', p_attribute_cd => 'EXAM_WIN_START') 
     AND 
     cencustlib.get_cust_attrdtl_dvar(p_cust_id => 'BOCATC', p_attribute_ty => 'EXAM_WIN_START', p_attribute_cd => 'EXAM_WIN_START')
-  AND m.cert_ty='ATHLETIC_TRAINER'
-  AND m.level_id='EXAM_ELIGIBLE'
-  AND s.collection_id = 'AT_INITIAL_APP'
-  AND wkfcfglib.getcurrentstate(s.wkf_serno) in ('EXAM_ELIGIBLE')
+) AND NOT EXISTS (
+  SELECT 1 FROM SBM_SUBMITTAL s
+  WHERE s.collection_id='AT_INITIAL_EXAM'
+  AND wkfcfglib.getcurrentstate(s.wkf_serno) in ('EXAM_REGISTERED')
+  AND s.primary_cust_id = :cust_id
 )
