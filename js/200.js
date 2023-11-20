3b2db240d00f0d785c533cf613fcf4ff9d3a4ab9
James Higgins <jamesh@bocatc.org>
2023-11-20 14:59:02 -0600

    taking file back from ACGI size() to length calls)

--- js/200.js
+++ js/200.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
   create_menu()
   update_login_placeholder()
   addCreatedToSubmittalTable()
   expand_school_codes();
-  
+
   replaceElementsStr('table', 'Certification Maintenance Fee (Renewal)', ' January   01, 2020', ' December 31, 2021')
   replaceElementsStr('table', 'Certification Maintenance Fee (Renewal)', ' January   01, 2021', ' December 31, 2021')
-  
+
   replaceElementsStr('table', 'Certification Maintenance Fee (Renewal)', '01/01/2020', '12/31/2021')
   replaceElementsStr('table', 'Certification Maintenance Fee (Renewal)', '01/01/2021', '12/31/2021')
-  
+
   replaceElementsStr('td', 'BOCATC - Certified', '12/31/2021-12/31/2020', '1/1/2020-12/31/2020')
   replaceElementsStr('td', 'BOCATC - Certified', '12/31/2021-12/31/2021', '1/1/2021-12/31/2021')
 
   replaceElementsStr('div', 'You do not yet have any submissions', 'You do not yet have any submissions', '')
 
-  
+
   changeCeFormDates();
 });
 
@@ -27,9 +27,9 @@ $(document).ajaxComplete(function () {
 });
 
 function apAnnualReportHideText() {
-  
-  if ($("#ap-initial-app-awaiting-pymnt").size() > 0) {
-    $("#ap-annual-report-paid").addClass('hide-it')
+
+  if ($("#ap-initial-app-awaiting-pymnt").length > 0) {
+      $("#ap-annual-report-paid").addClass('hide-it')
   }
 }
 
@@ -37,19 +37,19 @@ function apAnnualReportHideText() {
 // Iterate over HTML table and insert the date into each IDs corresponding row.
 function addCreatedToSubmittalTable() {
   if ($("#submittal_created_dates").size() > 0) {
-    $("th.aaSbmNumber").parent().children().eq(4).after("<th>Created Date</th>")
-    var pairs = $("#submittal_created_dates").text().split("::")
-    var id_date_map = {}
+      $("th.aaSbmNumber").parent().children().eq(4).after("<th>Created Date</th>")
+      var pairs = $("#submittal_created_dates").text().split("::")
+      var id_date_map = {}
 
-    for (i = 0; i < pairs.length; i++) {
-      var split = pairs[i].split("--")
-      id_date_map[split[0]] = split[1]
-    }
+      for (i = 0; i < pairs.length; i++) {
+          var split = pairs[i].split("--")
+          id_date_map[split[0]] = split[1]
+      }
 
-    $("td.aaSbmNumber").each(function( index ) {
-      var date_str = "<td>" + id_date_map[$(this).text()] + "</td>"
-      $(this).parent().children().eq(4).after( date_str)
-    });
+      $("td.aaSbmNumber").each(function (index) {
+          var date_str = "<td>" + id_date_map[$(this).text()] + "</td>"
+          $(this).parent().children().eq(4).after(date_str)
+      });
   }
 }
 
@@ -64,28 +64,28 @@ function changeCeFormDates() {
   startDateCorrectly = (startDate.includes("2022") || startDate.includes("2023") || startDate.includes("12/31/2021"))
 
   if (isPending && isFiftyDue && dateEndsCorrectly && startDateCorrectly) {
-    reportingPeriod.html("Reporting Period: 1/1/2022 - 12/31/2023");
+      reportingPeriod.html("Reporting Period: 1/1/2022 - 12/31/2023");
   }
 }
 
 function expand_school_codes() {
-  $("[headers^=C6906227190725890]").each(function() {
-    var t1 = $(this).text().replace("-B", "-B (Bachelor's)");
-    t1 = t1.replace("-M", "-M (Master's)");
-    $(this).text(t1);
+  $("[headers^=C6906227190725890]").each(function () {
+      var t1 = $(this).text().replace("-B", "-B (Bachelor's)");
+      t1 = t1.replace("-M", "-M (Master's)");
+      $(this).text(t1);
   })
 
-  $("[headers^=C7266359656361870]").each(function() {
-    var t1 = $(this).text().replace("-B", "-B (Bachelor's)");
-    t1 = t1.replace("-M", "-M (Master's)");
-    $(this).text(t1);
+  $("[headers^=C7266359656361870]").each(function () {
+      var t1 = $(this).text().replace("-B", "-B (Bachelor's)");
+      t1 = t1.replace("-M", "-M (Master's)");
+      $(this).text(t1);
   })
 }
 
 function update_login_placeholder() {
   var input = $("#P101_USERNAME")
   if (input.length > 0) {
-    input.attr("placeholder", "Username (typically your email)");
+      input.attr("placeholder", "Username (typically your email)");
   }
 }
 
@@ -95,8 +95,8 @@ function update_login_placeholder() {
 function replaceElementsStr(containerType, mustHaveStr, targetStr, newStr) {
   ele = $(containerType + ":contains('" + mustHaveStr + "'):contains('" + targetStr + "')").last()
   if (ele.length > 0) {
-    $(ele).html(ele.html().replace(targetStr, newStr))
-  } 
+      $(ele).html(ele.html().replace(targetStr, newStr))
+  }
 }
 
 function create_menu() {
@@ -105,20 +105,20 @@ function create_menu() {
   // Select all the menu elements (<li>) and split them in half
   var menu_items = $("#BOCMenuNavLinks li")
   var top_ul = $("#BOCMenuNavLinks ul").first()
-  
+
   const half = Math.ceil(menu_items.length / 2);
   const firstHalf = menu_items.slice(0, half)
   const secondHalf = menu_items.slice(-half)
 
   if (firstHalf.length > 0) {
-    top_ul.html("<div id='boc-menu-col0' class='boc-menu-col'></div><div id='boc-menu-col1' class='boc-menu-col'></div>")
-    $("#boc-menu-col0").html(firstHalf);
-    $("#boc-menu-col1").html(secondHalf);
+      top_ul.html("<div id='boc-menu-col0' class='boc-menu-col'></div><div id='boc-menu-col1' class='boc-menu-col'></div>")
+      $("#boc-menu-col0").html(firstHalf);
+      $("#boc-menu-col1").html(secondHalf);
   }
 }
 
 function changeNewSubmittalBtnLabel() {
+  if ($("#aaFormSubmit_ATHLETIC_TRAINER").length == 0) return;
   btn_value = $("#aaFormSubmit_ATHLETIC_TRAINER").attr("value");
   $("#aaFormSubmit_ATHLETIC_TRAINER").val(btn_value.replace("Create New", "Open"))
 }
-
